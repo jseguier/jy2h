5e8add822e00ce633b996d7905a004f280e3ed79
jseguier <rfkhftxnwf@privaterelay.appleid.com>
2025-11-21 21:34:28 +0100

    marker update

--- public/js/map.js
+++ public/js/map.js
@@ -1,13 +1,24 @@
 function init() { 
+    // Initialisation carte Leaflet
     const IUTCoords = [43.34216594139032, 5.412780422391714];
     const zoomLevel = 16.5;
     const map = L.map('map').setView(IUTCoords, zoomLevel);
 
+    // Obligation
     const mainLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' });
     mainLayer.addTo(map);
 
-    const marker = L.marker([43.34214, 5.412821], { icon: jy2h/public/img/JY2H-npm_ping.png }).addTo(map);
-    marker.bindPopup(`JY2h-npm-01`).openPopup();
+    // Marqueur JY2H-npm-01
+    const Icon = L.icon({iconUrl: 'public/img/JY2H-npm_ping.png', iconSize: [30, 30], popupAnchor: [0, -40]});
+
+    const marker = L.marker([43.34214, 5.412821], { icon: Icon }).addTo(map);
+
+    // Animation marqueur + texte
+    marker.bindPopup(`JY2h-npm-01`);
+    marker.on('mouseover', function () {this.openPopup();});
+    marker.on('mouseout', function () {this.closePopup();});
+
+
 
 
 }
