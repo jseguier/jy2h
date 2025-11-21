ff34d9555213f26e19c7190359347d9bc6485778
jseguier <rfkhftxnwf@privaterelay.appleid.com>
2025-11-21 18:17:12 +0100

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,6 @@
 function init() { 
     const IUTCoords = [43.34216594139032, 5.412780422391714];
-    const zoomLevel = 17;
+    const zoomLevel = 16.5;
     const map = L.map('map').setView(IUTCoords, zoomLevel);
 
     const mainLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' });
