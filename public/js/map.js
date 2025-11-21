01cb8eed83e7b9d0165aa1359b950268baab6bce
jseguier <rfkhftxnwf@privaterelay.appleid.com>
2025-11-21 17:42:48 +0100

    changes

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,7 @@
 function init() { 
     const IUTCoords = [48.8566, 2.3522];
     const zoomLevel = 13;
-    const map = L.map('mapid').setView(IUTCoords, zoomLevel);
+    const map = L.map('map').setView(IUTCoords, zoomLevel);
 
     const mainLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' });
     mainLayer.addTo(map);
